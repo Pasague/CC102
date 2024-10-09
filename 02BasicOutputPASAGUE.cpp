5482dd352dc3e7b4ce3b85bce3e00099d658372c
Pasague <pasagueyatts@gmail.com>
2024-10-09 09:32:54 +0800

    Add files via upload
    Signed-off-by: Pasague <pasagueyatts@gmail.com>

---
+++ 02BasicOutputPASAGUE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "our lady of lourdes college foundation" << endl;
+	cout << "college of computer studies" << endl;
+	cout << "bachelor of science in computer science-1st year" << endl;
+	cout << "John Raymond Pasague" << endl;
+	cout << "p-1 brgy binanuaan talisay camarines norte" << endl;
+	
+	
+	
+return 0;
+}
---
+++ 04BasicInputRAYMOND.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string nickname;
+	string course;
+	string school;
+	
+	cout << "nickname:";
+	cin >> nickname;
+	cout << " course-year level:";
+	cin >> course;
+	cout << "school:";
+	cin >> school;
+	cout << " wow congrats" << nickname << course <<" is a nice course. and you are studying in";
+	
+	return 0;
+}
