daf387aa23eb3a17dbe191d78fd9abfb2cb7cd6a
John Raymond pasague <pasagueyatts@gmail.com>
2024-11-22 10:07:28 +0800

    Add files via upload
    Signed-off-by: John Raymond pasague <pasagueyatts@gmail.com>

---
+++ Temperature.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+ {
+	float temperature;
+	cout << "what is the temperature ";
+	cin >> temperature;
+	if (temperature == 32) 
+		cout << " bring a heavy jacket";
+	
+	else if (temperature == 32) 
+		cout  << "bring a light jacket";
+	
+	else if (temperature == 50)	
+			cout << "not to bring any jacket";
+			
+return 0;
+
+	}
